387b0e51ec314cc05593ec3bcf6ab709f79abf36
Pex-Dev <brayanc.moya@gmail.com>
2025-09-05 17:43:58 -0400

    Added logo and unit selecto button

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,15 @@
+import logo from "../../assets/images/logo.svg";
+import UnitsSelector from "../UI/UnitsSelector";
+
 export default function Header() {
   return (
-    <header>
-      <h1 className="text-white">Weather Now</h1>
+    <header className="flex justify-between">
+      <img
+        src={logo}
+        alt="Weather now"
+        className="max-w-[150px] md:max-w-max "
+      />
+      <UnitsSelector />
     </header>
   );
 }
