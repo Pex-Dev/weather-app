48839aea942c555ea47d7086a12fd30068252bce
Pex-Dev <brayanc.moya@gmail.com>
2025-09-05 17:46:05 -0400

    Added classes to styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import Header from "./components/layout/Header";
 
 function App() {
   return (
-    <>
+    <div className="p-4 max-w-[1220px] mx-auto md:mt-8">
       <Header />
-    </>
+    </div>
   );
 }
 
