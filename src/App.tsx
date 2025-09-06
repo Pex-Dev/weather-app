d774b43e49e4a033d8225e62827cdce1cb054809
Pex-Dev <brayanc.moya@gmail.com>
2025-09-06 14:56:37 -0400

    Created title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/layout/Header";
+import Title from "./components/layout/Title";
 
 function App() {
   return (
     <div className="p-4 max-w-[1220px] mx-auto md:mt-8">
       <Header />
+      <Title />
     </div>
   );
 }
---
+++ src/components/layout/Title.tsx
@@ -0,0 +1,7 @@
+export default function Title() {
+  return (
+    <h1 className="font-bricolage-grotesque text-6xl text-center text-white font-semibold mt-14">
+      How's the sky looking today?
+    </h1>
+  );
+}
