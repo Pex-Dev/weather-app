56da804b6ca79703608e7657f77f699a052f592c
Pex-Dev <brayanc.moya@gmail.com>
2025-09-06 15:21:55 -0400

    Added Searchbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/layout/Header";
 import Title from "./components/layout/Title";
+import SearchBar from "./components/UI/SearchBar";
 
 function App() {
   return (
     <div className="p-4 max-w-[1220px] mx-auto md:mt-8">
       <Header />
       <Title />
+      <SearchBar />
     </div>
   );
 }
