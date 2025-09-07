de83d5f6e2c8cb6a1ba37532c45568682d816392
Pex-Dev <brayanc.moya@gmail.com>
2025-09-07 15:15:57 -0300

    Detailed info added

--- src/components/layout/Dashboard.tsx
+++ src/components/layout/Dashboard.tsx
@@ -1,9 +1,16 @@
 import MainInfoCard from "../UI/MainInfoCard";
+import DetailedInfoCard from "../UI/DetailedInfoCard";
 
 export default function Dashboard() {
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <MainInfoCard />
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+        <DetailedInfoCard label="Feels Like" value="64°" />
+        <DetailedInfoCard label="Humidity" value="46%°" />
+        <DetailedInfoCard label="Wind" value="9 mph" />
+        <DetailedInfoCard label="Precipitation" value="0" />
+      </div>
     </div>
   );
 }
