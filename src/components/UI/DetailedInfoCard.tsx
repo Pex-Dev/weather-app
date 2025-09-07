4ab1da1b23cdf23eb5a1103be22220b64583fcb3
Pex-Dev <brayanc.moya@gmail.com>
2025-09-07 15:15:32 -0300

    Detailed info card created

---
+++ src/components/UI/DetailedInfoCard.tsx
@@ -0,0 +1,14 @@
+export default function DetailedInfoCard({
+  label,
+  value,
+}: {
+  label: string;
+  value: string;
+}) {
+  return (
+    <div className="flex flex-col gap-4 bg-ui-main border border-ui-main-border rounded-xl p-4">
+      <h3 className="text-white/80">{label}</h3>
+      <p className="text-white text-3xl">{value}</p>
+    </div>
+  );
+}
