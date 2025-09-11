1dc54e0e415832020d9bdec022510b4fe716f01c
Pex-Dev <brayanc.moya@gmail.com>
2025-09-11 12:22:58 -0300

    Results type added

--- src/types/Types.tsx
+++ src/types/Types.tsx
@@ -3,3 +3,31 @@ export type Units = {
   wind: "kmh" | "mph";
   precipitation: "mm" | "inch";
 };
+
+export type SearchResults = {
+  results: Result[];
+  generationtime_ms: number;
+};
+
+export type Result = {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+  elevation: number;
+  feature_code: string;
+  country_code: string;
+  admin1_id: number;
+  admin2_id: number;
+  admin3_id?: number;
+  admin4_id?: number;
+  timezone: string;
+  population: number;
+  country_id?: number;
+  country?: string;
+  admin1: string;
+  admin2: string;
+  admin3?: string;
+  admin4?: string;
+  postcodes?: string[];
+};
