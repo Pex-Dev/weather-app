155e35a543d5e0309155ca7a6e6c65b30b8ebfa5
Pex-Dev <brayanc.moya@gmail.com>
2025-09-12 14:37:21 -0300

    Show message when there is no results

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,23 @@ import Header from "./components/layout/Header";
 import Title from "./components/layout/Title";
 import SearchBar from "./components/UI/SearchBar";
 import Dashboard from "./components/layout/Dashboard";
+import { UseWeatherContext } from "./context/WeatherAppContext";
 
 function App() {
+  const { searchStatus } = UseWeatherContext();
+
   return (
     <div className="p-4 max-w-[1220px] mx-auto md:mt-8">
       <Header />
       <Title />
       <SearchBar />
-      <Dashboard />
+      {searchStatus === "no-results" ? (
+        <h2 className="text-white text-4xl text-center mt-10">
+          No search result found
+        </h2>
+      ) : (
+        <Dashboard />
+      )}
     </div>
   );
 }
