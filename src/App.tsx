5d3f625d91662c2460488c1858a64f8a1aec9ad6
Pex-Dev <brayanc.moya@gmail.com>
2025-09-12 14:43:35 -0300

    display message when an error occurs

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from "./components/layout/Header";
 import Title from "./components/layout/Title";
 import SearchBar from "./components/UI/SearchBar";
 import Dashboard from "./components/layout/Dashboard";
+import Error from "./components/layout/Error";
 import { UseWeatherContext } from "./context/WeatherAppContext";
 
 function App() {
@@ -11,14 +12,20 @@ function App() {
   return (
     <div className="p-4 max-w-[1220px] mx-auto md:mt-8">
       <Header />
-      <Title />
-      <SearchBar />
-      {searchStatus === "no-results" ? (
-        <h2 className="text-white text-4xl text-center mt-10">
-          No search result found!
-        </h2>
+      {searchStatus === "error" ? (
+        <Error />
       ) : (
-        <Dashboard />
+        <>
+          <Title />
+          <SearchBar />
+          {searchStatus === "no-results" ? (
+            <h2 className="text-white text-4xl text-center mt-10">
+              No search result found!
+            </h2>
+          ) : (
+            <Dashboard />
+          )}
+        </>
       )}
     </div>
   );
