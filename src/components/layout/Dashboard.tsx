9ec15f4432d700e5658948bb2ee038370f14bea4
Pex-Dev <brayanc.moya@gmail.com>
2025-09-13 16:22:37 -0300

    Added current waether info and display selected units

--- src/components/layout/Dashboard.tsx
+++ src/components/layout/Dashboard.tsx
@@ -3,18 +3,62 @@ import DetailedInfoCard from "../UI/DetailedInfoCard";
 import DailyForecastCard from "../UI/DailyForecastCard";
 import HourlyForecastCard from "../UI/HourlyForecastCard";
 import DaySelector from "../UI/DaySelector";
+import { UseWeatherContext } from "../../context/WeatherAppContext";
 
 export default function Dashboard() {
+  const { weather, searchStatus, units } = UseWeatherContext();
+
+  if (searchStatus === "error" || searchStatus === "idle") return;
+
   return (
     <div className="flex flex-col lg:flex-row gap-4 mt-15 mb-15">
       <div className="flex flex-col gap-5">
         <MainInfoCard />
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-2">
-          <DetailedInfoCard label="Feels Like" value="64°" />
-          <DetailedInfoCard label="Humidity" value="46%°" />
-          <DetailedInfoCard label="Wind" value="9 mph" />
-          <DetailedInfoCard label="Precipitation" value="0" />
+          <DetailedInfoCard
+            label="Feels Like"
+            value={
+              searchStatus === "loading"
+                ? "-"
+                : weather
+                ? `${Math.round(weather.current.temperature_2m)}°`
+                : ""
+            }
+          />
+          <DetailedInfoCard
+            label="Humidity"
+            value={
+              searchStatus === "loading"
+                ? "-"
+                : weather
+                ? `${Math.round(weather.current.relative_humidity_2m)}%`
+                : ""
+            }
+          />
+          <DetailedInfoCard
+            label="Wind"
+            value={
+              searchStatus === "loading"
+                ? "-"
+                : weather
+                ? `${Math.round(weather.current.wind_speed_10m)} ${units.wind}`
+                : ""
+            }
+          />
+          <DetailedInfoCard
+            label="Precipitation"
+            value={
+              searchStatus === "loading"
+                ? "-"
+                : weather
+                ? `${Math.round(weather.current.precipitation)} ${
+                    units.precipitation
+                  }`
+                : ""
+            }
+          />
         </div>
+
         <h3 className="text-white text-xl font-light mt-3">Daily Forecast</h3>
         <div className="grid grid-cols-3 md:grid-cols-7 gap-4">
           <DailyForecastCard day="Tue" icon="sunny" max={20} min={14} />
