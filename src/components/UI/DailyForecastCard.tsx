1ab6d7a2c8c8a24272f308f9977fd938a80acef4
Pex-Dev <brayanc.moya@gmail.com>
2025-09-14 16:15:26 -0300

    Display icon for the waether of that day

--- src/components/UI/DailyForecastCard.tsx
+++ src/components/UI/DailyForecastCard.tsx
@@ -1,49 +1,26 @@
-import iconSunny from "../../assets/images/icon-sunny.webp";
-import iconPartlyCloudy from "../../assets/images/icon-partly-cloudy.webp";
-import iconCloudy from "../../assets/images/icon-overcast.webp";
-import iconDrizzle from "../../assets/images/icon-drizzle.webp";
-import iconRainy from "../../assets/images/icon-rain.webp";
-import iconSnowy from "../../assets/images/icon-snow.webp";
-import iconStormy from "../../assets/images/icon-storm.webp";
-import iconFoggy from "../../assets/images/icon-fog.webp";
+import { WeatherCodes } from "../../utilities/Utilities";
 
 export default function DailyForecastCard({
   day,
-  icon = "sunny",
+  weatherCode,
   max,
   min,
 }: {
   day: string;
-  icon?:
-    | "sunny"
-    | "partlyCloudy"
-    | "cloudy"
-    | "drizzle"
-    | "rainy"
-    | "snowy"
-    | "stormy"
-    | "foggy";
+  weatherCode: number;
   max: number;
   min: number;
 }) {
-  const weathericon = {
-    sunny: iconSunny,
-    partlyCloudy: iconPartlyCloudy,
-    cloudy: iconCloudy,
-    drizzle: iconDrizzle,
-    rainy: iconRainy,
-    snowy: iconSnowy,
-    stormy: iconStormy,
-    foggy: iconFoggy,
-  };
-
   return (
-    <div className="bg-ui-main border border-ui-main-border rounded-xl p-3 text-white">
+    <div className="bg-ui-main border border-ui-main-border rounded-xl p-3 text-white min-w-[96px]">
       <h4 className="text-lg text-center">{day}</h4>
-      <img src={weathericon[icon]} alt="iconSunny" />
+      <img
+        src={`/images/weather_icons/${WeatherCodes[weatherCode].icons.day}`}
+        alt={WeatherCodes[weatherCode].name}
+      />
       <p className="flex justify-between w-full text-lg">
-        <span>{max}°</span>
-        <span>{min}°</span>
+        <span>{Math.round(max)}°</span>
+        <span>{Math.round(min)}°</span>
       </p>
     </div>
   );
