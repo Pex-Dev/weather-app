447961a1c5a6b5182eee37d9cbcd4eae6b6aecdd
Pex-Dev <brayanc.moya@gmail.com>
2025-09-15 16:18:31 -0300

    Use real forecast parameters

--- src/components/UI/DaySelector.tsx
+++ src/components/UI/DaySelector.tsx
@@ -1,29 +1,17 @@
 import iconDropdown from "../../assets/images/icon-dropdown.svg";
-import UnitsDropdown from "./UnitsDropdown";
 import { useEffect, useRef, useState } from "react";
 
-export default function DaySelector() {
-  const [selectedDay, setSelectedDay] = useState<
-    | "Monday"
-    | "Tuesday"
-    | "Wednesday"
-    | "Thursday"
-    | "Friday"
-    | "Saturday"
-    | "Sunday"
-  >("Tuesday");
+export default function DaySelector({
+  days,
+  selectedDay,
+  setSelectedDay,
+}: {
+  days?: string[];
+  selectedDay?: Date;
+  setSelectedDay?: React.Dispatch<React.SetStateAction<Date | null>>;
+}) {
   const [showDropdown, setShowDropdown] = useState<Boolean>(false);
   const daysDiv = useRef<HTMLDivElement>(null);
-
-  const days = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ];
 
   //Close menu when click outside
   useEffect(() => {
@@ -36,24 +24,52 @@ export default function DaySelector() {
     window.addEventListener("mousedown", HandleClickOutside);
   }, [showDropdown]);
 
+  const DateFormater = (rawDate: Date): string => {
+    return new Date(rawDate).toLocaleDateString("en-US", {
+      weekday: "long",
+    });
+  };
+
+  const RemoveDuplicates = (dates: string[]) => {
+    const days: number[] = [];
+    const fixedDates: Date[] = [];
+    dates.map((date) => {
+      if (!days.includes(new Date(date).getDate())) {
+        days.push(new Date(date).getDate());
+        fixedDates.push(new Date(date));
+      }
+    });
+    return fixedDates;
+  };
+
   return (
     <div ref={daysDiv} className="relative">
       <button
-        onClick={() => setShowDropdown((prev) => !prev)}
+        onClick={() => {
+          if (!days) return;
+          setShowDropdown((prev) => !prev);
+        }}
         className="bg-ui-main-border hover:cursor-pointer transition-colors flex justify-between gap-1.5 items-center py-1 md:py-2 px-3 md:px-4 rounded-md md:rounded-lg"
       >
-        <span className=" text-white font-light">{selectedDay}</span>
+        <span className=" text-white font-light">
+          {selectedDay
+            ? new Date(selectedDay).toLocaleDateString("en-US", {
+                weekday: "long",
+              })
+            : "-"}
+        </span>
         <img src={iconDropdown} alt="Icon dropdown" className="max-w-3" />
       </button>
-      {showDropdown && (
+      {showDropdown && days && setSelectedDay && (
         <div className="absolute mt-3 right-0 bg-ui-main border border-ui-main-border rounded-md md:rounded-xl p-2 min-w-[212px] z-10 shadow-2xl">
-          {days.map((day) => (
+          {RemoveDuplicates(days).map((day) => (
             <label className="block w-full px-2 py-1 rounded md:rounded-md text-white font-extralight hover:bg-ui-main-hover">
-              {day}
+              {DateFormater(new Date(day))}
               <input
                 type="radio"
                 name="day"
-                value={day}
+                value={day.toDateString()}
+                onClick={() => setSelectedDay(day)}
                 className="appearance-none"
               />
             </label>
