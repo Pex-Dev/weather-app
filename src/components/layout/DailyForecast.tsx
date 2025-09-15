4cc36fb556f89f969c6c57455019320202eb5d22
Pex-Dev <brayanc.moya@gmail.com>
2025-09-15 16:20:31 -0300

    Created DailyForecast to separate from Dashboard

---
+++ src/components/layout/DailyForecast.tsx
@@ -0,0 +1,41 @@
+import DailyForecastCard from "../UI/DailyForecastCard";
+import { UseWeatherContext } from "../../context/WeatherAppContext";
+
+export default function DailyForecast() {
+  const { weather, searchStatus } = UseWeatherContext();
+
+  const DateFormater = (rawDate: Date): string => {
+    return new Date(rawDate).toLocaleDateString("en-US", {
+      weekday: "short",
+    });
+  };
+
+  return (
+    <>
+      <h3 className="text-white text-xl font-light mt-3">Daily Forecast</h3>
+      <div className="grid grid-cols-3 md:grid-cols-7 gap-4">
+        {searchStatus === "loading" ? (
+          <>
+            <div className="bg-ui-main min-w-[102px] min-h-[158px] border border-ui-main-border rounded-xl"></div>
+            <div className="bg-ui-main min-w-[102px] min-h-[158px] border border-ui-main-border rounded-xl"></div>
+            <div className="bg-ui-main min-w-[102px] min-h-[158px] border border-ui-main-border rounded-xl"></div>
+            <div className="bg-ui-main min-w-[102px] min-h-[158px] border border-ui-main-border rounded-xl"></div>
+            <div className="bg-ui-main min-w-[102px] min-h-[158px] border border-ui-main-border rounded-xl"></div>
+            <div className="bg-ui-main min-w-[102px] min-h-[158px] border border-ui-main-border rounded-xl"></div>
+            <div className="bg-ui-main min-w-[102px] min-h-[158px] border border-ui-main-border rounded-xl"></div>
+          </>
+        ) : (
+          weather?.daily.time.map((time, i) => (
+            <DailyForecastCard
+              key={i}
+              day={DateFormater(time)}
+              weatherCode={weather ? weather.daily.weather_code[i] : 0}
+              max={weather ? weather.daily.temperature_2m_max[i] : 0}
+              min={weather ? weather.daily.temperature_2m_min[i] : 0}
+            />
+          ))
+        )}
+      </div>
+    </>
+  );
+}
