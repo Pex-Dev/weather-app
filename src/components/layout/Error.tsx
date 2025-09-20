424233d42086a562f961e0c8bfb58b917565af40
Pex-Dev <brayanc.moya@gmail.com>
2025-09-20 15:24:28 -0300

    Fixed svg property names

--- src/components/layout/Error.tsx
+++ src/components/layout/Error.tsx
@@ -1,6 +1,9 @@
 import iconError from "../../assets/images/icon-error.svg";
+import { UseWeatherContext } from "../../context/WeatherAppContext";
 
 export default function Error() {
+  const { tryAgain } = UseWeatherContext();
+
   return (
     <div className="mx-auto w-fit flex flex-col items-center text-white mt-20 md:mt-25">
       <img src={iconError} alt="Icon error" className="w-10" />
@@ -11,7 +14,10 @@ export default function Error() {
         We couldn't connect to the server (API error). Please try again in a few
         moments
       </p>
-      <button className="flex gap-1 items-center mt-5 bg-ui-main py-1.5 px-3 rounded-md hover:cursor-pointer hover:bg-ui-main-hover transition-colors">
+      <button
+        className="flex gap-1 items-center mt-5 bg-ui-main py-1.5 px-3 rounded-md hover:cursor-pointer hover:bg-ui-main-hover transition-colors"
+        onClick={tryAgain}
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="22"
@@ -19,9 +25,9 @@ export default function Error() {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="1"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         >
           <path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" />
           <path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" />
