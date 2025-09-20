bfd81880df9dad325f4a16931e9a932b4d9bcae2
Pex-Dev <brayanc.moya@gmail.com>
2025-09-20 15:09:48 -0300

    ReverseGeocoding type added

--- src/types/Types.tsx
+++ src/types/Types.tsx
@@ -113,3 +113,33 @@ export type Icons = {
   day: string;
   night: string;
 };
+
+export type ReverseGeocoding = {
+  place_id: number;
+  licence: string;
+  osm_type: string;
+  osm_id: number;
+  lat: string;
+  lon: string;
+  class: string;
+  type: string;
+  place_rank: number;
+  importance: number;
+  addresstype: string;
+  name: string;
+  display_name: string;
+  address: Address;
+  boundingbox: string[];
+};
+
+export type Address = {
+  road: string;
+  neighbourhood: string;
+  city: string;
+  county: string;
+  state: string;
+  "ISO3166-2-lvl4": string;
+  postcode: string;
+  country: string;
+  country_code: string;
+};
