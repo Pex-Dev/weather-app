47eaf50805d83b3a81557930dd5218f366101967
Pex-Dev <brayanc.moya@gmail.com>
2025-09-23 16:33:13 -0300

    Classname fixes

--- src/components/layout/current/DetailedInfoCard.tsx
+++ src/components/layout/current/DetailedInfoCard.tsx
@@ -6,7 +6,7 @@ export default function DetailedInfoCard({
   value: string | number;
 }) {
   return (
-    <div className="flex flex-col gap-4 bg-ui-main border border-ui-main-border rounded-xl p-4 w-full lg:max-w-[180px]">
+    <div className="flex flex-col gap-4 bg-ui-main border border-ui-main-border rounded-xl p-4 w-full">
       <h3 className="text-white/80">{label}</h3>
       <p className="text-white text-3xl">{value}</p>
     </div>
