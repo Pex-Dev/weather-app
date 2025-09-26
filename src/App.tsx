8e19dfaf8dfa0e7814970c8e102882935da54a20
Pex-Dev <brayanc.moya@gmail.com>
2025-09-26 18:17:36 -0300

    Display compare screen when is a location in the list of locations to compare

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import Title from "./components/layout/Title";
 import SearchBar from "./components/UI/SearchBar";
 import Dashboard from "./components/layout/Dashboard";
 import Error from "./components/layout/Error";
+import CompareLocations from "./components/layout/compare/CompareLocations";
 import { UseWeatherContext } from "./context/WeatherAppContext";
 
 function App() {
-  const { searchStatus } = UseWeatherContext();
+  const { searchStatus, locationsToCompare } = UseWeatherContext();
 
   return (
     <div className="p-4 max-w-[1220px] mx-auto">
@@ -16,14 +17,20 @@ function App() {
         <Error />
       ) : (
         <>
-          <Title />
-          <SearchBar />
-          {searchStatus === "no-results" ? (
-            <h2 className="text-white text-4xl text-center mt-10">
-              No search result found!
-            </h2>
+          {locationsToCompare !== null ? (
+            <CompareLocations />
           ) : (
-            <Dashboard />
+            <>
+              <Title />
+              <SearchBar showNoResultsScreen={true} favoriteButton={true} />
+              {searchStatus === "no-results" ? (
+                <h2 className="text-white text-4xl text-center mt-10">
+                  No search result found!
+                </h2>
+              ) : (
+                <Dashboard />
+              )}
+            </>
           )}
         </>
       )}
